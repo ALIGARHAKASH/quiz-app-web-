b8fb9960735ba02e072d5b4d4f96491541655ae5
ALIGARHAKASH <80601103+ALIGARHAKASH@users.noreply.github.com>
2021-03-14 02:24:21 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "In which year of First World War Germany declared war on Russia and France?",
+    answer: "1914",
+    options: [
+      "1914",
+      "1915",
+      "1916",
+      "1912"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Eritrea, which became the 182nd member of the UN in 1993, is in the continent of",
+    answer: "africa",
+    options: [
+      "asia",
+      "africa",
+      "europe",
+      "australia"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does ffc stand for?",
+    answer: "Film Finance Corporation",
+    options: [
+      "Film Finance Corporation",
+      "File Finance Corporation",
+      "Freelancer Finance Corporation",
+      "Federation of Football Council"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
